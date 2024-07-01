c058924a2526058946f93abceb08020eff9f5fd5
Torugam <joelomarmena@gmail.com>
2024-07-01 11:24:03 -0600

    pantalla de recovery password hecha - diseño

--- src/screens/RecoveryPasswordScreen.js
+++ src/screens/RecoveryPasswordScreen.js
@@ -1,46 +1,94 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput as RNTextInput, ImageBackground, TouchableOpacity } from 'react-native';
+import { Button, PaperProvider } from 'react-native-paper';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
 
 
-const RecoveryPasswordScreen = ({ logueado, setLogueado}) => {
-    return ( 
+const RecoveryPasswordScreen = ({ logueado, setLogueado }) => {
+  const [email, setEmail] = React.useState('');
+  const navigation = useNavigation();
+
+  const handleForgotVerification = () => {
+    // Navegar a la pantalla de recuperación de contraseña
+    navigation.navigate('VerificationCode');
+  };
+
+  return (
+    <PaperProvider>
+      <ImageBackground source={require('../../assets/fondo-change.png')} style={styles.backgroundImage}>
         <View style={styles.container}>
-        <Text style={styles.title}>
-        Pantalla de la recuperación de contraseña xd
-        </Text>
-        <Text style={styles.descripcion}>
-            Ejemplo de consumo de API externa utilizando la función <Text style={styles.negrita}>FETCH</Text>
-        </Text>
+        <Image source={require('../../assets/icon-correo.png')} style={styles.logo} />
+          <Text style={styles.textLabel}>Ingrese su correo</Text>
+          <View style={styles.inputContainer}>
+            <Ionicons name="mail-outline" size={20} style={styles.icon} />
+            <RNTextInput
+              placeholder="correo electrónico "
+              value={email}
+              onChangeText={setEmail}
+              style={styles.input}
+            />
+          </View>
+          <Button mode="contained" onPress={handleForgotVerification} style={styles.button}>
+            Enviar
+          </Button>
         </View>
-     );
+      </ImageBackground>
+
+    </PaperProvider>
+  );
 }
- 
+
 export default RecoveryPasswordScreen;
 
 
 // Estilos para los componentes.
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      paddingTop: 20,
-      paddingHorizontal:15
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      marginTop: 10,
-      textTransform: 'uppercase',
-    },
-    descripcion: {
-        fontSize: 16,
-        fontWeight: '400',
-        textAlign: 'justify',
-        marginTop: 10,
-      },
-      negrita:{
-        fontWeight:'bold'
-      }
-  });
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  textLabel: {
+    fontSize: 16,
+    color: '#000',
+    fontWeight: 'bold',
+    textAlign: 'rigth',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: '#777',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+  },
+  logo: {
+    marginBottom: 150,
+  },
+  button: {
+    width: '100%',
+    paddingVertical: 10,
+    marginTop: 10,
+    backgroundColor: 'black',
+  }
+});
   
